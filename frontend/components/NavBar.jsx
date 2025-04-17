d94832851f00c4177dad6dfe35126084cce08d94
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-04-16 20:02:45 -0600

    Update NavBar.jsx
    Added home a href to the nav bar

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -8,6 +8,7 @@ export default function NavBar() {
                 <a href="/" className="text-4xl font-bold text-[oklch(0.359_0.144_278.697)] hover:text-blue-500">Quizar</a>
                 <img src={quasar} alt="Logo"  className='h-6 w-14 mt-3 ml-1 object-cover rounded-lg'/>
                 <ul className="flex flex-wrap gap-4 text-xl justify-between ml-auto">
+                    <li><a href="/" className="text-[oklch(0.359_0.144_278.697)] hover:text-blue-500">Home</a></li>
                     <li><a href="/Quiz" className="text-[oklch(0.359_0.144_278.697)] hover:text-blue-500">Quiz</a></li>
                     <li><a href="/Analytics" className="text-[oklch(0.359_0.144_278.697)] hover:text-blue-500">Analytics</a></li>
                     <li><a href="/Profile" className="text-[oklch(0.359_0.144_278.697)] hover:text-blue-500">Profile</a></li>
