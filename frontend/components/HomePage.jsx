b0bfaa1e15b26efc4cf0438769d8ee9c509a5e3d
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-04-16 20:03:51 -0600

    Update HomePage.jsx
    updated some styling to the home page

--- frontend/components/HomePage.jsx
+++ frontend/components/HomePage.jsx
@@ -10,7 +10,7 @@ import Collaboration from './Collaboration.jsx';
 
 
 
-export default function PortalExample() {
+export default function HomePage() {
   return (
     <>
     {/* Background Color:oklch(0.141_0.005_285.823)
@@ -19,8 +19,8 @@ export default function PortalExample() {
       <NavBar/>
 
       <div className="flex items-center justify-center h-50 bg-[oklch(0.141_0.005_285.823)]">
-        <div className="flex items-center justify-center bg-black-100 p-3 rounded-lg hover:scale-105 transition-transform hover:text-blue-500 duration-300">
-          <a href="/Quiz" className="flex w-42 py-2 px-2 items-center justify-center border-double text-xl text-white border-5 rounded-lg border-blue-500 hover:border-blue-300">Take ACE Quiz</a>
+      <div className="flex items-center justify-center bg-black-100 p-3 rounded-lg hover:scale-105 transition-transform hover:text-blue-500 duration-300">
+      <a href="/Quiz" className="flex w-42 py-2 px-2 items-center justify-center border-double text-xl text-white border-5 rounded-lg border-blue-500 hover:border-blue-300">Take ACE Quiz</a>
         </div>
       </div>
       
@@ -33,8 +33,7 @@ export default function PortalExample() {
         <Card.Text className="text-md text-white ml-1 mb-5"> 
           View your performances and track your progress.
         </Card.Text>
-        <a href="/Analytics" variant="dark" className=" text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500">Go Analytics</a>
-      </Card.Body>
+        <a href="/Analytics" variant="dark" className=" text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500">Go Analytics</a>      </Card.Body>
       <Card.Footer className="mt-4">
         <small className="text-muted ml-2 mt-10">Last updated 3 mins ago</small>
       </Card.Footer>
@@ -46,8 +45,7 @@ export default function PortalExample() {
     <Card.Text className="text-md text-white ml-1 mb-5">
           Don't want to take a Quiz? Just want to review the questions we got you.
         </Card.Text>
-        <a href="/Questions" variant="dark" className="  text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500">Go Questions</a>
-      </Card.Body>
+        <a href="/Questions" variant="dark" className="  text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500">Go Questions</a>      </Card.Body>
       <Card.Footer className='mb-2 mt-5'>
       <small className="text-muted ml-2">Last updated 3 mins ago</small>
       </Card.Footer>
@@ -59,10 +57,9 @@ export default function PortalExample() {
     <Card.Text className="text-md text-white ml-1 mb-5">
           Didn't find what you were looking for? Submit a feature request.
         </Card.Text>
-        <a  className="  text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500 cursor-pointer">Go Requests</a>
-      </Card.Body>
-      <Card.Footer className='mb-2 mt-5'>
-      <small className="text-muted ml-2">Last updated 3 mins ago</small>
+        <a  className="  text-blue-500 items-center justify-center mt-2 mb-20 rounded-2xl text-2xl py-2 px-4 transition-colors duration-500 border-black bg-gray-900 hover:border-1 hover:border-blue-500 cursor-pointer">Go Requests</a>      </Card.Body>
+        <Card.Footer className='mb-2 mt-5'>    
+            <small className="text-muted ml-2">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
       </CardGroup>
